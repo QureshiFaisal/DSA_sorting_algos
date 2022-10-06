cf27b5ae28d720e6e4c67df6badd24404007bb85
QureshiFaisal <85436772+QureshiFaisal@users.noreply.github.com>
2022-10-06 15:15:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -38,15 +38,13 @@
 
 // console.log(selectionSort([5, 4, 3, 2, 1]));
 
-function insertionSort(arr) {
-  for (i = 1; i < arr.length; i++) {
-    let currentVal = arr[i];
-    for (j = i - 1; j >= 0 && arr[j] > currentVal; j--) {
-      arr[j + 1] = arr[j];
+const insertionSort = (arr) => {
+  for (let i = 1; i< arr.length; i++){
+    let current = arr[i];
+    for(let j = i-1; j >=0 && arr[j] > current; j--){
+      arr[j+1] = arr[j];      
+      arr[j] = current;
     }
-    arr[j + 1] = currentVal;
   }
   return arr;
 }
-
-console.log(insertionSort([2, 1, 9, 76, 4]));
